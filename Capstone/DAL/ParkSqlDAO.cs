ab389657e98e894817da8617872cfed553e7c7aa
Richard SheavesBein <richard.sheavesbein@gmail.com>
2019-02-20 16:11:15 -0500

    capstone 2 ParkDAO

--- Capstone/DAL/ParkSqlDAO.cs
+++ Capstone/DAL/ParkSqlDAO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using Capstone.Models;
 
@@ -7,9 +8,60 @@ namespace Capstone.DAL
 {
     public class ParkSqlDAO : IParkDAO
     {
+        private string connectionString;
+
+        // Single Parameter Constructor
+        public ParkSqlDAO(string ConnectionString)
+        {
+            connectionString = ConnectionString;
+        }
+
+        /// <summary>
+        /// Returns a list of all the parks in the database
+        /// </summary>
+        /// <returns></returns>
         public IList<ParkModel> GetParks()
         {
-            throw new NotImplementedException();
+            List<ParkModel> parks = new List<ParkModel>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("select * from park", conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        ParkModel park = ConvertReaderToPark(reader);
+                        parks.Add(park);
+                    }
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("An error occurred reading parks.");
+                Console.WriteLine(ex.Message);
+            }
+
+            return parks;
+        }
+
+        private ParkModel ConvertReaderToPark(SqlDataReader reader)
+        {
+            ParkModel park = new ParkModel();
+            park.Park_Id = Convert.ToInt32(reader["Park_Id"]);
+            park.Name = Convert.ToString(reader["Name"]);
+            park.Location = Convert.ToString(reader["Location"]);
+            park.Establish_Date = Convert.ToDateTime(reader["Establish_Date"]);
+            park.Area = Convert.ToInt32(reader["Area"]);
+            park.Visitors = Convert.ToInt32(reader["Visitors"]);
+            park.Description = Convert.ToString(reader["Description"]);
+
+            return park;
         }
     }
 }
