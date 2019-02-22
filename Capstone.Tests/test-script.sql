42e08047179d2c2714c354fd8cd55aac25d26c17
Allison Debeljak <and21@case.edu>
2019-02-22 10:49:57 -0500

    Moved lines in test script

--- Capstone.Tests/test-script.sql
+++ Capstone.Tests/test-script.sql
@@ -6,9 +6,6 @@ declare @newReservationId int = (select @@identity)
 
 declare @parkCount int = (select count(*) from park)
 
-declare @parkId int = 1
-declare @campgroundCount int = (select count(*) from campground where park_Id = @parkId)
-
 declare @campgroundId int = 1
 declare @campsiteCount int = (select count(*) from site where campground_Id = @campgroundId)
 
@@ -16,5 +13,8 @@ declare @newOpenFrom int = 3
 declare @newOpenTo int = 10
 insert into campground values (@parkId, 'doesnt matter', @newOpenFrom, @newOpenTo, 10.00)
 declare @newCampgroundId int = (select @@identity)
+
+declare @parkId int = 1
+declare @campgroundCount int = (select count(*) from campground where park_Id = @parkId)
 
 select @campsiteId as campsiteId, @newReservationId as newReservationId, @parkCount as parkCount, @parkId as parkID, @campgroundCount as campgroundCount, @campgroundId as campgroundId, @campsiteCount as campsiteCount, @newOpenFrom as newOpenFrom, @newOpenTo as newOpenTo, @newCampgroundId as newCampgroundId
